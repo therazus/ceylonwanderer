86a09a79fc749e24eb8519a78ab651e273c2bf61
therazus <rasula.20@cse.mrt.ac.lk>
2025-08-01 15:52:00 +0530

    fixing build issues

--- src/app/components/DestinationMap/location-marker.tsx
+++ src/app/components/DestinationMap/location-marker.tsx
@@ -1,5 +1,5 @@
 import { cn } from "../../lib/utils";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
 interface LocationMarkerProps {
